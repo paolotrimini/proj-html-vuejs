ea1c1a3084193a7ad5ccce949e4d60bee6512b3f
paolotrimini <paolo.trimini@gmail.com>
2021-04-28 11:24:31 +0200

    test hamburger menu

--- js/script.js
+++ js/script.js
@@ -4,14 +4,40 @@
         new Vue ({
             el: '#app',
             data: {
+                sidebarMenu: false,
+
                 images: [
                     { icon: 'img/', alt: 'Facebook' },
                     { icon: 'img/', alt: 'Twitter' },
                     { icon: 'img/', alt: 'Instagram' },
                     { icon: 'img/', alt: 'Linkedin' },
                     { icon: 'img/', alt: 'Youtube' }
+                ],
+
+                names: [
+
+                    'Home',
+                    'Culinary History',
+                    'Our Team',
+                    'Our Menu',
+                    'Takeout',
+                    'Bulletin',
+                    'Reservations',
+                    'WooCommerce Cart'
                 ]
             },
+
+            methods: {
+
+                hideSidebarNavMenu: function() {
+
+                    if (this.sidebarMenu === true) {
+                        this.sidebarMenu = false;
+                    } else {
+                        this.sidebarMenu = true;
+                    }
+                },
+            }
 
 
         })
